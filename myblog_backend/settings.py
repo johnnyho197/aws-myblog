6d10941e50b94015d3f577dad2e8ca6a7f0bf5c6
Huu Ho <huu.ho@sjsu.edu>
2024-07-16 12:14:00 -0700

    update settings.py

--- myblog_backend/settings.py
+++ myblog_backend/settings.py
@@ -80,8 +80,12 @@ CORS_ALLOWED_ORIGINS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myblogdjango',
+        'USER': 'admin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 

079f1ff16363b693539679db924995270cda91e1
Huu Ho <huu.ho@sjsu.edu>
2024-07-17 13:15:17 -0700

    added put endpoint

--- blog/views.py
+++ blog/views.py
@@ -33,6 +33,16 @@ class PostDetail(APIView):
         serializer = PostSerializer(post)
         return Response(serializer.data)
 
+    def put(self, request, pk):
+        post = self.get_object(pk)
+        if post is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     def delete(self, request, pk):
         post = self.get_object(pk)
         if post is None:
